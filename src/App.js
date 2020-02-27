6bccdc5479fb7c1ffb4e257d495ebc79a648b080
sotengboy <sotengboy@gmail.com>
2020-02-27 09:17:21 +0700

    Update Code

--- src/App.js
+++ src/App.js
@@ -1,22 +1,92 @@
 import React from 'react';
 import axios from 'axios';
-import {Link, Router} from '@reach/router';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from "react-router-dom";
+
+import Detail from './components/Detail';
+
 import logo from './logo.svg';
 import './css/App.css';
 
-class PodcastList extends React.Component {
+class PodCasts extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filterText: '',
+      inStockOnly: false
+    };
+    
+    this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
+  }
+
+  handleFilterTextChange(filterText) {
+    this.setState({
+      filterText: filterText
+    });
+  }
+  
+
   render() {
-	  const podcasts = this.props.podcasts;
     return (
       <div>
-        <SearchBar />
-		<div>
-			<ul>
-			{podcasts.map(podcast => 
-				<li>{podcast.title}</li>
-			)}
-			</ul>
-		</div>
+        <SearchBar
+          filterText={this.state.filterText}
+          onFilterTextChange={this.handleFilterTextChange}
+        />
+        <PodcastList
+          podcasts={this.props.podcasts}
+          filterText={this.state.filterText}
+        />
+      </div>
+    );
+  }
+}
+class PodcastRow extends React.Component {
+  render() {
+    const podcast = this.props.podcast;
+    
+    return (
+      <ul className="List-pods">
+        <li>
+          <table className="Tabel-pods">
+            <tr>
+              <td><img src={podcast.thumbnail} alt={podcast.title} width="150px" height="150px" /></td>
+              <td>
+                <h3>{podcast.title}</h3>
+                <p>{podcast.url}</p>
+                <Link to={"/detail/"+podcast.id}>Lihat >></Link>
+              </td>
+            </tr>
+          </table>
+        </li>
+      </ul>
+    );
+  }
+}
+class PodcastList extends React.Component {
+  render() {
+    const filterText = this.props.filterText;
+
+    const rows = [];
+
+    this.props.podcasts.forEach((podcast) => {
+      if (podcast.title.indexOf(filterText) === -1) {
+        return;
+      }
+      rows.push(
+        <PodcastRow
+          podcast={podcast}
+          key={podcast.title}
+        />
+      );
+    });
+    return (
+      <div>
+        {rows}
       </div>
     );
   }
@@ -36,13 +106,28 @@ class Header extends React.Component {
 }
 
 class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
+    this.handleInStockChange = this.handleInStockChange.bind(this);
+  }
+  
+  handleFilterTextChange(e) {
+    this.props.onFilterTextChange(e.target.value);
+  }
+  
+  handleInStockChange(e) {
+    this.props.onInStockChange(e.target.checked);
+  }
+  
   render() {
     return (
       <form>
         <input
           type="text"
           placeholder="Cari Podcast..."
-          
+          value={this.props.filterText}
+          onChange={this.handleFilterTextChange}
         /><button>Cari</button>
       </form>
     );
@@ -60,13 +145,21 @@ class App extends React.Component {
         const podcasts = res.data;
         this.setState({ podcasts });
       })
+      
   }
   render (){
     return (
       <div className="App">
         <Header />
         <Router>
-            <PodcastList path="/" podcasts={this.state.podcasts} />
+        <Switch>
+          <Route exact path="/">
+            <PodCasts podcasts={this.state.podcasts} />
+          </Route>
+          <Route path="/detail/:id">
+            <Detail />
+          </Route>
+        </Switch>
         </Router>
       </div>
     );
---
+++ src/components/Detail.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import axios from 'axios';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    useParams
+  } from "react-router-dom";
+  import '..//css/App.css';
+class Data extends React.Component {
+    
+    state = {
+        podcasts: []
+      }
+    
+      componentDidMount() {
+        const id = this.props.id;
+        axios.get(`https://json-server-heroku-svoqwyfacm.now.sh/podcasts?id=`+id)
+          .then(res => {
+            const podcasts = res.data;
+            this.setState({ podcasts });
+          })
+          
+      }
+    render() {
+        
+        return (
+          <div className="Detail-pod">
+            
+            <ul className="List-pods">
+                <li>
+                    <table className="Tabel-pods">
+                    { this.state.podcasts.map(podcast => 
+                        <tr>
+                            <td><img src={podcast.thumbnail} alt={podcast.title} width="350px"/></td>
+                            <td>
+                                <h3>{podcast.title}</h3>
+                                <p>{podcast.url}</p>
+                                Episodes:
+                                {podcast.episodes ? (
+                                        <Episodes episodes={podcast.episodes} />
+                                    ) : (
+                                        <p>Tidak ada episode</p>
+                                    )}
+                                    <a href="/">Kembali</a>
+                            </td>
+                        </tr>
+                    )}
+                    </table>
+                </li>
+            </ul>
+        
+          </div>  
+        );
+    }
+}
+class Episodes extends React.Component {
+    render(){
+        const episodes = this.props.episodes;
+    return (
+        <div>
+    {episodes.map(item => 
+        <p>
+            <audio controls>
+                <source src={item.audio}></source>
+            </audio></p>
+        )}
+        </div>
+    );
+    }
+}
+function Detail() {
+    let { id } = useParams();
+    return (
+        <Data id={ id } />
+    )
+}
+export default Detail;
